2bdaaebe4115f00eff213189c8d15f81af1bfeff
Yiğitcan YARDIMCI <147248981+Yigitcanyardimci@users.noreply.github.com>
2026-05-07 14:56:54 +0300

    Update performance.R

--- performance.R
+++ performance.R
@@ -1,1 +1,34 @@
-# 7
+measure_performance <- function(model, task, split) {
+  
+  preds <- model$predict(task, row_ids = split$test)
+  
+  c_index <- as.numeric(
+    preds$score(
+      mlr3::msr("surv.cindex"),
+      task = task
+    )
+  )[1]
+  
+  brier <- as.numeric(
+    preds$score(
+      mlr3::msr("surv.brier"),
+      task      = task,
+      train_set = split$train
+    )
+  )[1]
+  
+  auc <- as.numeric(
+    preds$score(
+      mlr3::msr("surv.uno_auc", integrated = TRUE),
+      task      = task,
+      train_set = split$train
+    )
+  )[1]
+  
+  return(list(
+    c_index = c_index,
+    brier   = brier,
+    auc     = auc,
+    preds   = preds
+  ))
+}
